27d577412018a9d0ca7a9dc9136c0b14770cdeb9
VladSvotin <dis4343@mail.ru>
2022-03-04 05:02:36 +0400

    дддд

---
+++ src/test/java/stepDefenition/Steps.java
@@ -0,0 +1,45 @@
+package stepDefenition;
+
+import io.cucumber.java.ru.Дано;
+import io.cucumber.java.ru.И;
+import io.cucumber.java.ru.Тогда;
+
+public class Steps {
+    int a;
+    int b;
+    double result;
+    String action;
+
+    @Дано("^два числа (.*) и (.*)$")
+    public void given(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Тогда("^находим (.*) двух числел$")
+    public void result(String operation) {
+        switch (operation) {
+            case "сложение":
+                this.action = "Сложение";
+                this.result = a + b;
+                break;
+            case "вычитание":
+                this.action = "Вычитание";
+                this.result = a - b;
+                break;
+            case "умножение":
+                this.action = "Умножение";
+                this.result = a * b;
+                break;
+            case "деление":
+                this.action = "Деление";
+                this.result = (float) a / b;
+                break;
+        }
+    }
+
+    @И("^выводим результат на экран$")
+    public void printResult() {
+        System.out.println(action + " чисел " + a + " и " + b + " ровняется " + result);
+    }
+}
